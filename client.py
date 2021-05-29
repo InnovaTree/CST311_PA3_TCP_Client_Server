cc3953d7cf27d0686510eb29461dad97effd20e2
raymondshum <rshum@csumb.edu>
2021-05-29 12:41:10 -0700

    Updated threading, connections
    Update
    
    recv_message will close its connection on receiving "Bye" from the server
    
    Join threads before ending program

--- client.py
+++ client.py
@@ -1,17 +1,41 @@
+"""
+
+"""
+
 from socket import *
+import threading
 
 # In your command prompt, type in hostname and press enter.
 # What comes up is your computer's hostname
-serverName = 'localhost'
+serverName = '127.0.0.1'
 serverPort = 12000
 
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
 
-sentence = input('Input lowercase sentence:')
+msg_recvd = False
+
+
+def recv_msg():
+    while True:
+        msg = clientSocket.recv(1024).decode()
+        if msg == "Bye":
+            clientSocket.send("Bye".encode())
+            clientSocket.close()
+            break
+        elif msg:
+            print(msg)
+            global msg_recvd
+            msg_recvd = True
+
+
+thread = threading.Thread(target=recv_msg)
+thread.start()
+
+while not msg_recvd:
+    pass
+
+sentence = input('Enter message to send to server: ')
 clientSocket.send(sentence.encode())
 
-modifiedSentence = clientSocket.recv(1024)
-print('From Server:', modifiedSentence.decode())
-
-clientSocket.close()
+thread.join()
