7b93e96ae1ec3a737d5c4743ccc991bfde25b655
raymondshum <rshum@csumb.edu>
2021-05-28 19:29:41 -0700

    Server checks for 2 connected clients.
    Revised provided code.

--- client.py
+++ client.py
@@ -4,10 +4,14 @@ from socket import *
 # What comes up is your computer's hostname
 serverName = 'localhost'
 serverPort = 12000
+
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
+
 sentence = input('Input lowercase sentence:')
 clientSocket.send(sentence.encode())
+
 modifiedSentence = clientSocket.recv(1024)
 print('From Server:', modifiedSentence.decode())
+
 clientSocket.close()
--- server.py
+++ server.py
@@ -6,14 +6,26 @@ serverPort = 12000
 # Create a TCP socket
 # Notice the use of SOCK_STREAM for TCP packets
 serverSocket = socket(AF_INET, SOCK_STREAM)
+
 # Assign IP address and port number to socket
 serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
+
 print('The server is ready to receive')
 
-while True:
+connections = []
+while len(connections) < 2:
     connectionSocket, addr = serverSocket.accept()
+    connections.append((connectionSocket,addr))
+    print("Client {0} has connected".format(len(connections)))
+    # server needs to associate handler thread with each client
+
+
+
+""" From base code
+while True:
     sentence = connectionSocket.recv(1024).decode()
     capitalizedSentence = sentence.upper()
     connectionSocket.send(capitalizedSentence.encode())
     connectionSocket.close()
+"""
